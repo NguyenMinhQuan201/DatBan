a34272249cb688f1159bbd61ebffcd859fa5bb15
Quan Nguyen <quannm2812@gmail.com>
2024-08-20 10:40:08 +0700

    thay đổi

--- DatBan/Controllers/AreasController.cs
+++ DatBan/Controllers/AreasController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
     [ApiController]
     public class AreasController : ControllerBase
     {
-        //thay đổi test
+        //thay đổi test 2
         private readonly IAreaService _Areaservice;
         public AreasController(IAreaService Areaservice)
         {
